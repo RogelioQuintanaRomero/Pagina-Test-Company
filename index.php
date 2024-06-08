72a85fd05327a5c13d0e991dd269d5d1e6a3eda7
Rogelio Quintana Romero <oilegor_rqr@hotmail.com>
2024-06-07 18:43:01 -0600

    carga de pagina completa

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="Descripcion" content="Coca cola. es la bebida">
+		<meta name="keywords" content="bebida, gaseosa, cola, soda">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>
+			Inicio - Coca Cola
+		</title>
+		<link rel="shortcut icon" href="img/favicon.png">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="style.css">
+		<script src="https://kit.fontawesome.com/f546e97529.js" crossorigin="anonymous"></script>
+
+	</head>
+<body>
+	<header>
+		<div class="container">
+			<a href="index.html">
+				<img src="img/logo.svg" class="logo">
+			</a>
+			<nav>
+				<a href="#inicio">Inicio</a>
+				<a href="#nosotros">Nosotros</a>
+				<a href="#servicios">Servicios</a>
+				<a href="#galeria">Galeria</a>
+				<a href="#contactenos">Contactenos</a>
+			</nav>	
+			<a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
+		</div>
+	</header>
+	<main>
+		<section id="inicio">
+			<img src="img/bannerprincipal.jpg">
+			<div class="bloque-inicio">
+				<h1>Bienvenidos a Coca-Cola</h1>
+				<p>
+				 Nam ac tellus varius, ali
+
+quam risus vitae, facilisis quam. In mattis nunc eu sapien fringilla, vitae consectetur urna convallis. Nam sit amet facilisis ipsum.
+				</p>
+				<a href="#nosotros" class="boton boton-rojo">
+				Ver más
+			</a>
+			</div>
+		</section>
+		<section id="nosotros" class="seccion">
+			<div class="container">
+				<p>
+					<h5>Sed nibh nulla</h5>
+					venenatis in elit nec, molestie lobortis diam. Etiam nec accumsan est. Donec ullamcorper leo sapien.
+					<h5>In mattis nunc eu</h5>			non ultricies metus faucibus id. Nam ac tellus varius, aliquam risus vstae, facilisis quam.
+					<h5>Donec risus dolor</h5>
+					 condimentum non fermentum ut, aliquet et sapien. Proin condimentum risus sem, at porttitor tortor interdum pellentesque.
+				</p>
+			</div>
+		</section>
+		<section id="servicios" class="seccion">
+			<div class="container">
+				<div class="row">
+					<div class="columna columna-33 columna-mobile-100">
+						<div class="bloque-servicio">
+							<div class="bloque-img-servicio cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+							</div>
+							<div class="bloque-contenido-servicio"> 
+								<h3>Servicio 1</h3>
+							<p>
+							Vestibulum aliquet leo id augue euismod faucibus. Nunc facilisis eget orci in tristique. Sed ultrices tempus eros vel cursus.
+							</p>
+							<a href="#" class="boton boton-blanco">Ver más</a>
+							</div>
+						</div>
+					</div>
+					<div class="columna columna-33 columna-mobile-100">
+						<div class="bloque-servicio">
+							<div class="bloque-img-servicio cuadrado-perfecto">
+								<img src="img/servicio2.jpg">
+							</div>
+							<div class="bloque-contenido-servicio"> 
+								<h3>Servicio 2</h3>
+							<p>
+							 Vivamus tincidunt, odio eu pretium luctus, risus turpis porta lorem, id feugiat lacus sapien non dolor. Etiam eu pulvinar velit.
+							</p>
+							<a href="#" class="boton boton-blanco">Ver más</a>
+							</div>
+						</div>
+					</div>
+					<div class="columna columna-33 columna-mobile-100">
+						<div class="bloque-servicio">
+							<div class="bloque-img-servicio cuadrado-perfecto">
+								<img src="img/servicio3.jpg">
+							</div>
+							<div class="bloque-contenido-servicio"> 
+								<h3>Servicio 3</h3>
+							<p>
+							 Donec risus dolor, condimentum non fermentum ut, aliquet et sapien. Proin condimentum risus sem, at porttitor tortor interdum pellentesque.	
+							</p>
+							<a href="#" class="boton boton-blanco">Ver más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>			
+		</section>
+		<section id="galeria" >
+			<div class="container-fluid">
+				<div class="row">
+					<div class=" columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio1.jpg">
+							<h4>Imagen1</h4>
+						</div>
+					</div>
+					<div class=" columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio2.jpg">
+							<h4>Imagen2</h4>
+						</div>
+					</div>
+					<div class=" columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio3.jpg">
+							<h4>Imagen3</h4>
+						</div>
+					</div>
+					<div class=" columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio1.jpg">
+							<h4>Imagen4</h4>
+						</div>
+					</div>
+					<div class=" columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio2.jpg">
+							<h4>Imagen5</h4>
+						</div>
+					</div>
+					<div class=" columna-33 columna-mobile-50">
+						<div class="cuadrado-perfecto">
+							<img src="img/servicio3.jpg">
+							<h4>Imagen6</h4>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</section>
+		<section id="contactenos" class="seccion">
+			<iframe width="520" height="400" frameborder="0"  src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Saltillo+(Mexico)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+						<form action="index.php" method="post">
+							<div class="form-block">
+								<input type="text" name="nombre" class="form-control" placeholder="Nombre"/>
+							</div>
+							<div class="form-block">
+								<input type="email" name="email" class="form-control"  placeholder="Email"/>
+							</div>
+							<div class="form-block">
+								<textarea name="mensaje" placeholder="Mensaje"></textarea>
+							</div>
+								<div class="form-block bloque-ultimo">
+								<input type="submit" class="boton btn-negro" value="Enviar"/>
+							</div>
+							<?php
+								if($_SERVER["REQUEST_METHOD"]=="POST")
+								{
+									$nombre = $_POST["nombre"];
+									$email = $_POST["email"];
+									$mensaje = $_POST["mensaje"];
+
+									if(isset($nombre))
+									{
+										if(isset($email))
+										{
+											if(isset($mensaje))
+											{
+												$para = "rogelio.quintana@outlook.com";
+												$asunto = "Contacto Pagina Web";
+												$cuerpo = $nombre."\n".$email."\n".$mensaje;
+												$adicional = "From : noreply@company.com";
+
+												if(mail($para, $asunto, $cuerpo, $adicional))
+													{
+												?>
+													<p>Envio Exitosos</p>
+												<?php
+													}
+												else 
+													{
+													?>
+										            <p>Email fallo al enviar...</p>
+										            <?php
+											    	}
+											}
+										}
+									}
+								}
+							?>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	<footer >
+		<div class="container">
+			<div class="row">
+				<div class="columna columna-25 columna-mobile-100 ">
+					<img src="img/logo-blanco.png" class="logo-footer">
+					<p>Morbi a vestibulum enim. Phasellus ut purus vitae ex auctor posuere. Donec at maximus neque, et finibus sapien.</p>
+				</div>
+				<div class="columna columna-25 columna-mobile-100 ">
+					<h3>
+						Temas relacionados
+					</h3>
+					<ul>
+						<li><a href="#">Tema 1</a></li>
+						<li><a href="#">Tema 2</a></li>
+						<li><a href="#">Tema 3</a></li>
+					</ul>
+				</div>
+				<div class="columna columna-25 columna-mobile-100 ">
+					<h3>
+						Datos de Contacto
+					</h3>
+					<ul>
+						<li>Correo: contacto@company.com</li>
+						<li>Telefono: (234)5345323</li>
+						<li>Direccion: Av. Universidad 3456 - Pueblo Libre<br>Lima, Peru</li>
+					</ul>
+				</div>
+				<div class="columna columna-25 columna-mobile-100 ">
+					<h3>
+						Redes Sociales
+					</h3>
+					<ul class="redes">
+						<li>
+							<a href="#">
+								<i class="fa-brands fa-square-facebook"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fa-brands fa-square-twitter"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fa-brands fa-instagram"></i>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="barra-footer">
+			&copy; Derechos Reservados - 2020
+		</div>
+	</footer>
+	<script type="text/javascript" src="js/jquery-3.7.1.min.js"></script>
+	<script src="js/funciones.js"></script>
+
+</body>
+</html>
